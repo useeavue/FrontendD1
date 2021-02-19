05239ebe478db05b5df313ce621aaaf22464e47f
Maksim Makarov <therealbrookebond@gmail.com>
2021-02-19 19:56:18 +0400

    deadline

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 let tab1 = document.querySelector('.movies'),
-  tab2 = document.querySelector('.television');
-let buttonTab1 = document.querySelector('#button-tab1'),
+  tab2 = document.querySelector('.television'),
+  loginForm = document.querySelector('.login-form'),
+  loginButton = document.querySelector('.login-button'),
+  closeButton = document.querySelector('.login-close-btn'),
+  buttonTab1 = document.querySelector('#button-tab1'),
   buttonTab2 = document.querySelector('#button-tab2');
 
-buttonTab1.onclick = function () {
-  tab1.classList.toggle('movies-toggle');
+loginButton.onclick = () => {
+  loginForm.classList.remove('login-form-off');
 };
 
-buttonTab2.onclick = function () {
-  tab2.classList.toggle('television-toggle');
+closeButton.onclick = () => {
+  loginForm.classList.add('login-form-off');
+};
+
+buttonTab2.onclick = () => {
+  tab1.classList.add('movies-toggle');
+  tab2.classList.add('television-toggle');
+  buttonTab1.classList.remove('button-tab1_style');
+  buttonTab2.classList.add('button-tab2_style');
+};
+
+buttonTab1.onclick = () => {
+  tab1.classList.remove('movies-toggle');
+  tab2.classList.remove('television-toggle');
+  buttonTab1.classList.add('button-tab1_style');
+  buttonTab2.classList.remove('button-tab2_style');
 };
